64e2ecbbc63ce8150c3860d3dc8884cbbb4700f5
IbraheemRodrigues <ibraheemrodrigues@gmail.com>
2017-09-05 11:19:47 +0100

    added raw type tp log module

--- log.py
+++ log.py
@@ -1,6 +1,8 @@
 import io, datetime
 
 states = {
+    -1 : "raw"
+
     0 : "info  ",
     1 : "warn  ",
     2 : "error ",
@@ -11,9 +13,12 @@ states = {
 def log(mesgType, message):
 
     timestamp = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-    mesgType = states[mesgType] if type(mesgType) == int else mesgType
+    mesgType = states[mesgType] if type(mesgType) == int else raise ValueError("Bad Message Type")
 
-    tolog = "@%s(UTC) : %s : \'%s\'\n" % (timestamp, mesgType, message)
+    if mesgType == "raw":
+        tolog = "\n\n@%s(UTC) : %s : \'%s\'\n\n\n" % (timestamp, mesgType, message)
+    else:
+        tolog = "@%s(UTC) : %s : \'%s\'\n" % (timestamp, mesgType, message)
 
     with open("serverLog.log", 'a') as log:
         log.write(tolog)
